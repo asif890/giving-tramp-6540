bc88d29a3bf275664134e8aece3c3435a3e4fd93
asif_ikbal <skasif10001@gmail.com>
2023-05-07 13:41:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 
 import './App.css';
-import logo from "./images/img.jpg"
+import Navbar from './Component/Navber';
 import Allroutes from './Component/Allroutes';
 
+
+
+
+
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <h1>asif ikbal</h1>
-      <img src={logo} alt='fon'/>
+      <Navbar/>
       <Allroutes/>
-    </div>
+     </div> 
   );
 }
-
 export default App;
