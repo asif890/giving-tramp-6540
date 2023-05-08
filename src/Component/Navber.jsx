69f8e835b9233a9488fedfc1e1a4078345a2ebe1
asif_ikbal <skasif10001@gmail.com>
2023-05-08 18:52:19 +0530

    done

--- src/Component/Navber.jsx
+++ src/Component/Navber.jsx
@@ -14,15 +14,15 @@ const Navbar=()=>{
      
 
 
-        <div  style={{border:"2px solid red" ,width:"100%",display:"flex",justifyContent:"space-between"}}>
+        <div  style={{width:"100%",display:"flex",justifyContent:"space-between"}}>
     <div><img src={logo} style={{width:"130px",height :"80px"}} alt="none"/></div>
         <div><h1>The Flower Shop</h1></div>    
 
-        <div style={{border:"2px solid blue",margin:"20px",display:"flex",justifyContent:"space-between"}}>
+        <div style={{margin:"20px",display:"flex",justifyContent:"space-between"}}>
             
    {
     links.map((e)=>{
-        return <div   style={{border:"2px solid red",margin:"5px"}}  ><Link  key={e.path} to={e.path}>{e.title }</Link></div>
+        return <div   style={{margin:"5px"}}  ><Link  key={e.path} to={e.path}>{e.title }</Link></div>
     })
    }
    
